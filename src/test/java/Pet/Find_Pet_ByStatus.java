72df3b63a5dd8fabf7a5e3a8dbd9abcfb5afb8ae
Raja Junaid Ullah Khan <junaidraja2@gmail.com>
2023-08-04 13:04:49 +0500

    New Update

---
+++ src/test/java/Pet/Find_Pet_ByStatus.java
@@ -0,0 +1,23 @@
+package Pet;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class Find_Pet_ByStatus {
+
+    String Status = "available";
+    //String FindByStatus = "sold";
+    //String FindByStatus = "pending";
+
+    @Test
+    public void FindByID() {
+        // Sending the POST request and getting the response
+        baseURI= "https://petstore.swagger.io/v2/";
+        given().
+                get("pet/findByStatus?status=" + Status )
+                // Asserting the status code to be 200 for successful creation
+                .then().statusCode(200);
+    }
+}
---
+++ src/test/java/Pet/Update_Pet.java
@@ -0,0 +1,41 @@
+package Pet;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+public class Update_Pet {
+
+    @Test
+    public void Update_Pet()
+
+    {
+        String Body = "{\n" +
+                "        \"id\": 0,\n" +
+                "        \"category\": {\n" +
+                "          \"id\": 0,\n" +
+                "          \"name\": \"string\"\n" +
+                "        },\n" +
+                "        \"name\": \"doggie\",\n" +
+                "        \"photoUrls\": [\n" +
+                "          \"string\"\n" +
+                "        ],\n" +
+                "        \"tags\": [\n" +
+                "          {\n" +
+                "            \"id\": 0,\n" +
+                "            \"name\": \"string\"\n" +
+                "          }\n" +
+                "        ],\n" +
+                "        \"status\": \"available\"\n" +
+                "     }";
+
+        // Sending the POST request and getting the response
+        baseURI = "https://petstore.swagger.io/v2/";
+        given().
+                header("Content-Type", "application/json").
+                body(Body).
+                // Asserting the status code to be 200 for successful creation
+                put("pet").then().statusCode(200);
+
+    }
+}
